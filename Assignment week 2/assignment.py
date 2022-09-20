dc2d9e952e56819f6ccb5c441707ea6aecf07254
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-20 18:53:53 +0300

    assignment 2

---
+++ Assignment week 2/assignment.py
@@ -0,0 +1,11 @@
+import re
+
+rsum = open(r'regex_sum.txt')
+numlist = list()
+for line in rsum:
+    line = line.rstrip()
+    stuff = re.findall('[0-9.]+', line)
+    if len(stuff) != 1: continue
+    for num in stuff:
+        numlist.append(int(num))
+print(sum(numlist))
