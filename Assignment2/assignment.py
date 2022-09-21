edb7acbe7e1b63fdfdc7dedad54940535a51f19a
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-21 03:47:47 +0300

    still making changes

--- Assignment2/assignment.py
+++ Assignment2/assignment.py
@@ -2,10 +2,11 @@ import re
 
 rsum = open("regex.txt")
 numlist = list()
+
 for line in rsum:
     line = line.rstrip()
     stuff = re.findall('[0-9]+', line)
-    if len(stuff) != 3: continue
+    if len(stuff) == 0: continue
     num = int(stuff[0])
     numlist.append(num)
 print(sum(numlist))
